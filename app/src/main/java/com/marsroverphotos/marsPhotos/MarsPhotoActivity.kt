7eb4514dcadb05f292f548e9d714950f0f210e0d
Valeriy Bodak <valery.bodak@exposit.com>
2019-08-09 12:56:40 +0300

    GridLayoutManager has been added instead

--- app/src/main/java/com/marsroverphotos/marsPhotos/MarsPhotoActivity.kt
+++ app/src/main/java/com/marsroverphotos/marsPhotos/MarsPhotoActivity.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.Observer
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.mars_photos_activity.*
@@ -13,6 +14,8 @@ import com.marsroverphotos.entities.Status
 
 class MarsPhotoActivity : AppCompatActivity() {
 
+    private val PHOTO_GALLERY_COLUMNS_NUMBER = 5
+
     private val photosViewModel: MarsPhotoViewModel by viewModel()
     private lateinit var listAdapter: MarsPhotoAdapter
 
@@ -21,7 +24,7 @@ class MarsPhotoActivity : AppCompatActivity() {
         setContentView(R.layout.mars_photos_activity)
         listAdapter = MarsPhotoAdapter()
 
-        recycler_view.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recycler_view.layoutManager = GridLayoutManager(this, PHOTO_GALLERY_COLUMNS_NUMBER)
         recycler_view.adapter = listAdapter
         photosViewModel.fetchNews()
     }
